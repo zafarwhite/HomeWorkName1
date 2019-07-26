001f03acffd79473b3276dc7c99654e920f49654
Zafar <34389538+zafarwhite@users.noreply.github.com>
2019-07-26 16:38:32 +0300

    Update HomeWorkName2.2.js

--- HomeWorkName2.2.js
+++ HomeWorkName2.2.js
@@ -1,9 +1,9 @@
-const primeNumber = () =>{
+const primeNumber = () => {
 	nextPrime: for (var i = 2; i <= 100; i++) {
 
 		for (var j = 2; j < i; j++) {
 			if (i % j == 0) continue nextPrime;
 		}
-		console.log( i ); // простое
+// 		console.log( i ); // простое
 	};
 }
