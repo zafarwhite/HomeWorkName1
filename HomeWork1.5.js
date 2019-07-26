426050e77b233ea732b6887ff06d8f53ea196b0e
Zafar <34389538+zafarwhite@users.noreply.github.com>
2019-07-26 16:24:09 +0300

    Update HomeWork1.5.js
    upgrade)

--- HomeWork1.5.js
+++ HomeWork1.5.js
@@ -1,27 +1,22 @@
-
-const BestMark    = 'A';
-const BetterMark  = 'B';
-const GoodMark    = 'C';
-const BadMark     = 'D';
-const VeryBadMark = 'E';
-const WorstMark   = 'F';
-
-
-const Rate = (rate) => {
+const bestMark    = 'A';
+const betterMark  = 'B';
+const goodMark    = 'C';
+const badMark     = 'D';
+const veryBadMark = 'E';
+const worstMark   = 'F';
+const getMarkByRate = (rate) => {
 	if(0<rate && rate<19){
-		console.log(`Your mark is ${WorstMark}`);
+		return `Your mark is ${WorstMark}`;
 	}else if(20<rate && rate<39){
-		console.log(`Your mark is ${VeryBadMark}`);
+		return `Your mark is ${VeryBadMark}`;
 	}else if(40<rate && rate<59){
-		console.log(`Your mark is ${BadMark}`);
+		return `Your mark is ${BadMark}`;
 	}else if(60<rate && rate<74){
-		console.log(`Your mark is ${GoodMark}`);
+		return `Your mark is ${GoodMark}`;
 	}else if(75<rate && rate<89){
-		console.log(`Your mark is ${BetterMark}`);
+		return `Your mark is ${BetterMark}`;
 	}else if(90<rate && rate<100){
-		console.log(`Your mark is ${BestMark}`);
+		return `Your mark is ${BestMark}`;
 	}
 };
-
-Rate(25);
 
